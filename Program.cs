872829d70b2dd9537ab515f4704a253723a54e94
Marcos Rocha <marcos.fellps@gmail.com>
2021-07-07 11:07:59 -0300

    aula1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" - Start - ");
+
+            new ContaCorrente();
+
+        }
+    }
+}
