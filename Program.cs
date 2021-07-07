c685d6888c1ee570e9a86b15740a42ee03d24797
Marcos Rocha <marcos.fellps@gmail.com>
2021-07-07 13:06:49 -0300

    Aula3

--- Program.cs
+++ Program.cs
@@ -8,8 +8,20 @@ namespace aula1
         {
             Console.WriteLine(" - Start - ");
 
-            new ContaCorrente();
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.saldo = 200;
+            Console.WriteLine(primeiraContaCorrente.saldo);
 
+            primeiraContaCorrente.saldo += 100;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.saldo = 50;
+
+            Console.WriteLine("primeira conta tem " + primeiraContaCorrente.saldo);
+            Console.WriteLine("segunda conta tem " + segundaContaCorrente.saldo);
+
+            Console.ReadLine();
         }
     }
 }
